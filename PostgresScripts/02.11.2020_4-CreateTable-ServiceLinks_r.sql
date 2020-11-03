2f408f7320b612a266dfa40ec31d75d64986c13a
Loginov Alexander <loginov.a@skbkontur.ru>
2020-11-03 11:20:49 +0500

    ренейминг

--- PostgresScripts/02.11.2020_4-CreateTable-ServiceLinks_r.sql
+++ PostgresScripts/02.11.2020_4-CreateTable-ServiceLinks_r.sql
@@ -2,5 +2,5 @@ CREATE TABLE server_links
 (
 id                  integer                 PRIMARY KEY                         NOT NULL,
 transport_stop_id   integer                 REFERENCES transport_stop (id)      NOT NULL,
-link                CHARACTER VARYING(50)                                       NOT NULL
+link                CHARACTER VARYING(100)                                       NOT NULL
 );
