82040e59791571fa1e28bbcc6df3eecdc9cca80c
Loginov Alexander <loginov.a@skbkontur.ru>
2020-11-03 19:52:33 +0500

    Финальный коммит этой задачи
    Настроена работа бд, написано readme как развернуть ее локально

--- bot/src/main/java/com/github/telegram/bot/repos/TransportStopRepository.java
+++ bot/src/main/java/com/github/telegram/bot/repos/TransportStopRepository.java
@@ -2,7 +2,6 @@ package com.github.telegram.bot.repos;
 
 import com.github.telegram.bot.db.TransportStop;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public interface TransportStopRepository extends JpaRepository<TransportStop, Integer> {
 }
